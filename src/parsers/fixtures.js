f9d7e8dea0d505a72263484ca05325d6d1b8c5e7
rubyrubenstahl <ruby@ruby-lighting.com>
2019-04-01 00:30:37 -0400

    Adds matrix and section info to fixtures

--- src/parsers/fixtures.js
+++ src/parsers/fixtures.js
@@ -2,9 +2,10 @@ module.exports = function parseFixtures($) {
     const fixtures = $('Item');
     const fixtureData = fixtures.map((i, fixture) => {
         const itemData = fixture.children[1];
+
         const attribs = { ...fixture.attribs, ...itemData.attribs };
 
-        return {
+        const data = {
             id: Number(attribs['Id']),
             name: attribs['Name'],
             manufacturer: attribs['Manufacturer'],
@@ -19,9 +20,31 @@ module.exports = function parseFixtures($) {
             screenId: Number(attribs['ScreenID']),
             displayName: attribs['DisplayName'],
             linkedTo: Number(attribs['LinkedTo']),
+            matrix: getMatrixData(fixture)
+        };
+
+        const isInMatrix = !!data.matrix;
+        const sectionData = isInMatrix ? fixture.parent.parent.parent.parent.attribs : fixture.parent.parent.attribs
+        data.section = {
+            name: sectionData['Name']
         }
+        return data;
     });
     return fixtureData;
+}
+
+function getMatrixData(fixtureEl) {
+
+    if (fixtureEl.parent.name !== "MatrixElement") {
+        return null;
+    }
+    const matrixEl = fixtureEl.parent.parent;
+    const attribs = { ...matrixEl.attribs, ...matrixEl.firstChild.attribs };
+
+    const matrixData = {
+        id: Number(attribs.Id),
+    }
+    return matrixData;
 }
 
 
